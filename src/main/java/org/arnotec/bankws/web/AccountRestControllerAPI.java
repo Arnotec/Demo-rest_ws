1e5e9f2b783fff0d4e91427f8560f30848fb9a09
Arnaud <tsofackarnaud@gmail.com>
2021-10-26 19:24:58 +0400

    REST Web Service - using RestController of Spring Boot - producing JSON and XML responses

---
+++ src/main/java/org/arnotec/bankws/web/AccountRestControllerAPI.java
@@ -0,0 +1,45 @@
+package org.arnotec.bankws.web;
+
+import org.arnotec.bankws.entities.Account;
+import org.arnotec.bankws.repositories.AccountRepository;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/bank")
+public class AccountRestControllerAPI {
+
+   private final AccountRepository accountRepository;
+
+    public AccountRestControllerAPI(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @GetMapping(path = "/accounts", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public List<Account> getAccountList() {
+        return accountRepository.findAll();
+    }
+
+    @GetMapping("/accounts/{id}")
+    public Account getAccount(@PathVariable("id") Long id) {
+        return accountRepository.findById(id).get();
+    }
+
+    @PostMapping("/accounts")
+    public Account create(Account account) {
+        return accountRepository.save(account);
+    }
+
+    @PutMapping("/accounts/{id}")
+    public Account update(Account account, @PathVariable("id") Long id) {
+        account.setId(id);
+        return accountRepository.save(account);
+    }
+
+   @DeleteMapping("/accounts/{id}")
+    public void delete(Long id) {
+        accountRepository.deleteById(id);
+    }
+}
