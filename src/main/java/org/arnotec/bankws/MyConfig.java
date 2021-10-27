55486bbc656c46f957807e37fed0001409777dd1
Arnaud <tsofackarnaud@gmail.com>
2021-10-27 16:30:30 +0400

    REST Web Service - using Spring Data Rest(@RepositoryRestResource) - Exposing Ids in resources

--- src/main/java/org/arnotec/bankws/MyConfig.java
+++ src/main/java/org/arnotec/bankws/MyConfig.java
@@ -1,17 +1,25 @@
 package org.arnotec.bankws;
 
+import org.arnotec.bankws.entities.Account;
 import org.arnotec.bankws.web.AccountRestJaxRSAPI;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @Configuration
 public class MyConfig {
 
-    @Bean
+    public MyConfig(RepositoryRestConfiguration restConfiguration) {
+        restConfiguration.exposeIdsFor(Account.class);
+    }
+
+    //    @Bean
     public ResourceConfig jerseyConfig() {
         ResourceConfig jerseyConfig = new ResourceConfig();
         jerseyConfig.register(AccountRestJaxRSAPI.class);
         return jerseyConfig;
     }
+
+
 }
